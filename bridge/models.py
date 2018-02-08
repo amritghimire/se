7e1f777d13dd3b7ad0be0a76483704c123c32af0
Your Name <you@example.com>
2018-02-09 01:35:52 +0545

    change in models

--- bridge/models.py
+++ bridge/models.py
@@ -1,3 +1,29 @@
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+class Name(models.Model):
+    name_text = models.CharField(max_length=20,help_text="Enter your Name")
+
+    def __str__(self):
+        return self.name_text
+class Slug(models.Model):
+    slug_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.slug_text
+class Summary(models.Model):
+    summary_text = models.CharField(max_length=20,help_text="Write Summary")
+
+    def __str__(self):
+        return self.summary_text
+class Description(models.Model):
+    description_text = models.CharField(max_length=20,help_text="Write Description")
+
+    def __str__(self):
+        return self.description_text
+class Id(models.Model):
+    id_is = models.IntegerField(help_text="Enter your id")
+
+class Image_for_detail(models.Model):
+    image = models.ImageField()
+
