77f80e3b1fb9a8aabd9eb070982c662b03d55678
Jagdis01 <jagdishshrestha10@gmail.com>
2018-02-17 14:51:06 +0545

    Edit func.js file

--- staticfiles/js/func.js
+++ staticfiles/js/func.js
@@ -4,9 +4,6 @@ document.getElementById("cbt").onclick = function () {
 document.getElementById("shut").onclick = function(){
                 location.href = "give location";
             };
-document.getElementById("shut").onclick = function(){
-                  location.href = "give location";
-              };
 document.getElementById("clbtn").onclick = function () {
             location.href = "give location";
             };
