cb1b909f5ed98d16ece9c5189b3869528e944fc0
Jagdis01 <jagdishshrestha10@gmail.com>
2018-02-17 14:20:17 +0545

    Add func.js file

---
+++ staticfiles/js/func.js
@@ -0,0 +1,12 @@
+document.getElementById("cbt").onclick = function () {
+                    location.href = "give location	";
+                    };
+document.getElementById("shut").onclick = function(){
+                location.href = "give location";
+            };
+document.getElementById("shut").onclick = function(){
+                  location.href = "give location";
+              };
+document.getElementById("clbtn").onclick = function () {
+            location.href = "give location";
+            };
