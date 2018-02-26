68ba13058268e48c27842b323b6580ad6047773c
Amrit Ghimire <iamritghimire@gmail.com>
2018-02-26 19:31:55 +0545

    Calculate score of relationship

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import uuid as uuid_lib
 from product_app.models import Product
 from userProfile_app.models import UserProfile
+from recommendation_app.models import Recommended, NotRecommended
 
 
 class Relationship(models.Model):
@@ -18,7 +19,7 @@ class Relationship(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     score = models.IntegerField(null=True, blank=True, editable=False)
     rating = models.PositiveSmallIntegerField(choices=SCORE_CHOICES, null=True, blank=True)
-    review = models.TextField()
+    review = models.TextField(blank=True, null=True)
 
     uuid = models.UUIDField(  # Used by the API to look up the record
         db_index=True,
@@ -27,4 +28,29 @@ class Relationship(models.Model):
     )
 
     def __str__(self):
-        return "%s rating for %s by %s" % (self.rating, self.product, self.author)
+        return "%s score for %s by %s" % (self.score, self.product, self.author)
+
+    def recalculate_score(self):
+        """
+        :return int:
+        """
+        score_under_calculation = 0
+        author_selected_category = [a.uuid for a in self.author.selected_category.all()]
+        author_selected_tag = [a.uuid for a in self.author.selected_tag.all()]
+        score_under_calculation += sum([10 for a in self.product.category.all() if a.uuid in author_selected_category])
+        score_under_calculation += sum([5 for a in self.product.tag.all() if a.uuid in author_selected_tag])
+        score_under_calculation += sum([4 for a in self.product.tag.all() if self.author in a.viewed.all()])
+        if Recommended.objects.filter(author=self.author, product=self.product).exists():
+            score_under_calculation += 7
+        if NotRecommended.objects.filter(author=self.author, product=self.product).exists():
+            score_under_calculation -= 7
+        if self.rating:
+            score_under_calculation += 2
+        if self.review:
+            score_under_calculation += 2
+
+        return score_under_calculation
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.score = self.recalculate_score()
+        super().save(force_insert, force_update, using, update_fields)
